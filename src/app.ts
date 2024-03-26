ecfa787da724f21bc3c43589bf5cb4556f9080d1
nayzflux <55506519+nayzflux@users.noreply.github.com>
2024-03-26 21:09:21 +0100

    feat: support smart trim

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV !== "production") {
-    const dotenv = require("dotenv");
-    dotenv.config();
+  const dotenv = require("dotenv");
+  dotenv.config();
 }
 
 import bodyParser from "body-parser";
@@ -42,11 +42,11 @@ pubClient.connect();
 subClient.connect();
 
 const io = new Server(httpServer, {
-    cors: {
-        origin: CLIENT_URL,
-        credentials: true,
-    },
-    adapter: createAdapter(pubClient, subClient),
+  cors: {
+    origin: CLIENT_URL,
+    credentials: true,
+  },
+  adapter: createAdapter(pubClient, subClient),
 });
 
 // Trust the headers set by the reverse proxy
@@ -61,18 +61,18 @@ app.set("trust proxy", 1);
  */
 
 app.use(
-    cors({
-        origin: CLIENT_URL,
-        methods: ["GET", "POST", "DELETE", "PATCH"],
-        credentials: true,
-    })
+  cors({
+    origin: CLIENT_URL,
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+    credentials: true,
+  })
 );
 
 // Define the rate limit options
 const limiter = rateLimit({
-    windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 500, // limit each IP to 100 requests per windowMs
-    message: "Too many requests from this IP, please try again later.",
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 500, // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
 });
 
 // Apply the rate limiter to all requests
@@ -84,23 +84,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const sessionStore = new PrismaSessionStore(prisma, {
-    checkPeriod: 60 * 60 * 1000,
-    dbRecordIdIsSessionId: true,
-    dbRecordIdFunction: undefined,
+  checkPeriod: 60 * 60 * 1000,
+  dbRecordIdIsSessionId: true,
+  dbRecordIdFunction: undefined,
 });
 
 const sessionMiddleware = session({
-    secret: SESSION_SECRET,
-    resave: true,
-    saveUninitialized: false,
-    cookie: {
-        secure: false,
-        httpOnly: true,
-        sameSite: true,
-        // 15 days
-        maxAge: 15 * 24 * 60 * 60 * 1000,
-    },
-    store: sessionStore,
+  secret: SESSION_SECRET,
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+    secure: false,
+    httpOnly: true,
+    sameSite: true,
+    // 15 days
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+  },
+  store: sessionStore,
 });
 
 app.use(sessionMiddleware);
@@ -116,21 +116,21 @@ io.engine.use(sessionMiddleware);
 io.engine.use(passport.authenticate("session"));
 
 io.on("connection", (socket) => {
-    // @ts-ignore
-    console.log(socket.request.isAuthenticated());
+  // @ts-ignore
+  console.log(socket.request.isAuthenticated());
 
-    // @ts-ignore
-    if (!socket.request.isAuthenticated()) {
-        socket.disconnect();
-        return;
-    }
+  // @ts-ignore
+  if (!socket.request.isAuthenticated()) {
+    socket.disconnect();
+    return;
+  }
 
-    // @ts-ignore
-    const user = socket.request.user as User;
+  // @ts-ignore
+  const user = socket.request.user as User;
 
-    socket.join(`user:${user.id}`);
+  socket.join(`user:${user.id}`);
 
-    console.log("[WEBSOCKET] " + user.name);
+  console.log("[WEBSOCKET] " + user.name);
 });
 
 /**
@@ -149,87 +149,89 @@ app.use("/public", express.static("uploads/"));
 
 // Configuration de Multer pour le stockage des vidéos
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/scenes");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    },
+  destination: function (req, file, cb) {
+    cb(null, "uploads/scenes");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 
 const upload = multer({
-    storage: storage,
-    fileFilter: async function (req, file, cb) {
-        if (req.body.secret === process.env.PROCESS_WH_SECRET) {
-            return cb(null, true);
-        }
+  storage: storage,
+  fileFilter: async function (req, file, cb) {
+    if (req.body.secret === process.env.PROCESS_WH_SECRET) {
+      return cb(null, true);
+    }
 
-        cb(new Error("Invalid secret"));
-    },
+    cb(new Error("Invalid secret"));
+  },
 });
 
 app.post("/webhook/process", upload.single("file"), async (req, res) => {
-    const { event, video_id, scenes } = req.body;
+  const { event, video_id, scenes } = req.body;
 
-    console.log(event);
+  console.log(event, video_id);
 
-    try {
-        if (event === "PROCESS_FAILED") {
-            const video = await prisma.video.update({
-                where: { id: video_id },
-                data: {
-                    status: "FAILED",
-                },
-            });
+  try {
+    if (event === "PROCESS_FAILED") {
+      const video = await prisma.video.update({
+        where: { id: video_id },
+        data: {
+          status: "FAILED",
+        },
+      });
 
-            io.to(`user:${video.user_id}`).emit("video_process_failed");
+      io.to(`user:${video.user_id}`).emit("video_process_failed");
 
-            console.log("[WEBHOOK] Video process failed");
-        }
-
-        if (event === "PROCESS_SUCCEED") {
-            const video = await prisma.video.update({
-                where: { id: video_id },
-                data: {
-                    status: "PROCESSED",
-                    scene_urls: scenes.map((_: any, i: number) => "Scene" + i),
-                },
-            });
-
-            io.to(`user:${video.user_id}`).emit("video_process_success");
-
-            extractZip(
-                `uploads/scenes/${video.id}.zip`,
-                `uploads/scenes/${video.id}`
-            );
-
-            console.log("[WEBHOOK] Video process succeed");
-
-            res.sendStatus(200);
-        }
-
-        if (event === "PROCESS_STARTED") {
-            const video = await prisma.video.update({
-                where: { id: video_id },
-                data: {
-                    status: "PROCESSING",
-                },
-            });
-
-            io.to(`user:${video.user_id}`).emit("video_process_start");
-
-            console.log("[WEBHOOK] Video process started");
-
-            res.sendStatus(200);
-        }
-    } catch (err) {
-        console.error(err);
-        res.sendStatus(500);
+      console.log("[WEBHOOK] Video process failed");
     }
+
+    if (event === "PROCESS_SUCCEED") {
+      const video = await prisma.video.update({
+        where: { id: video_id },
+        data: {
+          status: "PROCESSED",
+          scene_urls: scenes.map((scene: any, i: number) =>
+            JSON.stringify(scene)
+          ),
+        },
+      });
+
+      io.to(`user:${video.user_id}`).emit("video_process_success");
+
+      extractZip(
+        `uploads/scenes/${video.id}.zip`,
+        `uploads/scenes/${video.id}`
+      );
+
+      console.log("[WEBHOOK] Video process succeed");
+
+      res.sendStatus(200);
+    }
+
+    if (event === "PROCESS_STARTED") {
+      const video = await prisma.video.update({
+        where: { id: video_id },
+        data: {
+          status: "PROCESSING",
+        },
+      });
+
+      io.to(`user:${video.user_id}`).emit("video_process_start");
+
+      console.log("[WEBHOOK] Video process started");
+
+      res.sendStatus(200);
+    }
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
 });
 
 httpServer.listen(PORT, () =>
-    console.log(`[*] Server is listening on port :${PORT}`)
+  console.log(`[*] Server is listening on port :${PORT}`)
 );
 
 export { io };
--- src/services/zipServices.ts
+++ src/services/zipServices.ts
@@ -1,5 +1,4 @@
 import AdmZip from "adm-zip";
-import * as fs from "fs";
 
 export const extractZip = (zipFilePath: string, extractTo: string) => {
   const zip = new AdmZip(zipFilePath);
