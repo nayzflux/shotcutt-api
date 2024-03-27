cae012cd9291c1e4867c17c1da19dc2b869ed049
nayzflux <55506519+nayzflux@users.noreply.github.com>
2024-03-27 19:04:32 +0100

    feat: add margin to cut

--- src/controllers/videoController.ts
+++ src/controllers/videoController.ts
@@ -9,115 +9,120 @@ import fs from "node:fs";
 import { deleteFile } from "../services/deleteService";
 
 export const createVideo: RequestHandler = async (req, res) => {
-    const user = req.user as User;
+  const user = req.user as User;
 
-    const file = req.file;
-    const preset = req.query.preset as string;
+  const file = req.file;
+  const preset = req.query.preset as string;
+  const cuttingMargin = (req.query.cuttingMargin || "0") as string;
 
-    console.log(req.query);
+  if (!preset) return res.sendStatus(400);
 
-    if (!preset) return res.sendStatus(400);
+  const cuttingMarginParsed = parseFloat(cuttingMargin) || 0;
 
-    if (
-        !(
-            preset === "MEDIUM" ||
-            preset === "FAST" ||
-            preset === "FASTER" ||
-            preset === "VERYFAST" ||
-            preset === "SUPERFAST"
-        )
+  if (cuttingMarginParsed > 0.5) return res.sendStatus(400);
+
+  if (
+    !(
+      preset === "MEDIUM" ||
+      preset === "FAST" ||
+      preset === "FASTER" ||
+      preset === "VERYFAST" ||
+      preset === "SUPERFAST"
     )
-        return res.sendStatus(400);
+  )
+    return res.sendStatus(400);
 
-    if (!file) return res.sendStatus(400);
+  if (!file) return res.sendStatus(400);
 
-    const video = await prisma.video.create({
-        data: {
-            user_id: user.id,
-            name: file.originalname,
-            url: `/public/originals/${file.filename}`,
-            scene_urls: [],
-            status: "WAITING",
-            size: file.size,
-            preset: preset,
-            filename: file.filename,
-            format: path.extname(file.originalname),
-        },
-    });
+  const video = await prisma.video.create({
+    data: {
+      user_id: user.id,
+      name: file.originalname,
+      url: `/public/originals/${file.filename}`,
+      scene_urls: [],
+      status: "WAITING",
+      size: file.size,
+      preset: preset,
+      filename: file.filename,
+      format: path.extname(file.originalname),
+    },
+  });
 
-    // Rename file for easier usage
-    fs.renameSync(
-        `uploads/originals/${req.file?.filename}`,
-        "uploads/originals/" + video.id + ".mp4"
-    );
+  // Rename file for easier usage
+  fs.renameSync(
+    `uploads/originals/${req.file?.filename}`,
+    "uploads/originals/" + video.id + ".mp4"
+  );
 
-    processingService.processVideo(video, file.filename);
+  processingService.processVideo(video, file.filename, cuttingMarginParsed);
 
-    res.status(201).json({ video });
+  res
+    .status(201)
+    .json({ video: { cuttingMargin: cuttingMarginParsed, ...video } });
 };
 
 export const findAllVideos: RequestHandler = async (req, res) => {
-    const user = req.user as User;
+  const user = req.user as User;
 
-    const videos = await prisma.video.findMany({
-        where: {
-            user_id: user.id,
-        },
-        orderBy: {
-            created_at: "desc",
-        },
-    });
+  const videos = await prisma.video.findMany({
+    where: {
+      user_id: user.id,
+    },
+    orderBy: {
+      created_at: "desc",
+    },
+  });
 
-    res.status(200).json({ videos });
+  res.status(200).json({ videos });
 };
 
 export const findVideo: RequestHandler = async (req, res) => {
-    const user = req.user as User;
-    const { id } = req.params;
+  const user = req.user as User;
+  const { id } = req.params;
 
-    const video = await prisma.video.findFirst({
-        where: {
-            user_id: user.id,
-            id,
-        },
-    });
+  const video = await prisma.video.findFirst({
+    where: {
+      user_id: user.id,
+      id,
+    },
+  });
 
-    if (!video) {
-        return res.sendStatus(404);
-    }
+  if (!video) {
+    return res.sendStatus(404);
+  }
 
-    res.status(200).json({ video });
+  res.status(200).json({ video });
 };
 
 export const deleteVideo: RequestHandler = async (req, res) => {
-    const user = req.user as User;
-    const { id } = req.params;
+  const user = req.user as User;
+  const { id } = req.params;
 
-    try {
-        const video = await prisma.video.delete({
-            where: {
-                user_id: user.id,
-                id,
-                OR: [{ status: "PROCESSED" }],
-            },
-        });
+  try {
+    const video = await prisma.video.delete({
+      where: {
+        user_id: user.id,
+        id,
+        OR: [{ status: "PROCESSED" }],
+      },
+    });
 
-        if (video.status === "WAITING") {
-            return res.sendStatus(409);
-        }
-
-        if (video.status === "PROCESSING") {
-            return res.sendStatus(409);
-        }
-
-        res.status(200).json({ video });
-
-        deleteFile(video);
-    } catch (err) {
-        if (err instanceof PrismaClientKnownRequestError) {
-            if (err.code === "P2025") return res.sendStatus(404);
-        }
-
-        res.sendStatus(500);
+    if (video.status === "WAITING") {
+      return res.sendStatus(409);
     }
+
+    if (video.status === "PROCESSING") {
+      return res.sendStatus(409);
+    }
+
+    res.status(200).json({ video });
+
+    deleteFile(video);
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      if (err.code === "P2025") return res.sendStatus(404);
+    }
+
+    res.sendStatus(500);
+  }
 };
--- src/services/processingService.ts
+++ src/services/processingService.ts
@@ -4,95 +4,95 @@ import rabbitmq from "../lib/rabbitmq";
 import { io } from "../app";
 import { prisma } from "../lib/prisma";
 
-export const processVideo = async (video: Video, filename: string) => {
-    try {
-        const connection = await rabbitmq;
-        const channel = await connection.createChannel();
+export const processVideo = async (video: Video, filename: string, cuttingMargin: number) => {
+  try {
+    const connection = await rabbitmq;
+    const channel = await connection.createChannel();
 
-        await channel.assertQueue("processing-queue", {
-            durable: true,
-        });
+    await channel.assertQueue("processing-queue", {
+      durable: true,
+    });
 
-        channel.sendToQueue(
-            "processing-queue",
-            Buffer.from(JSON.stringify(video))
-        );
+    channel.sendToQueue(
+      "processing-queue",
+      Buffer.from(JSON.stringify({ cuttingMargin, ...video }))
+    );
 
-        channel.close();
+    channel.close();
 
-        console.log("[PROCESS SERVICE] Video processing queued");
+    console.log("[PROCESS SERVICE] Video processing queued");
 
-        // await prisma.video.update({
-        //     where: { id: video.id },
-        //     data: { status: "PROCESSING" },
-        // });
-
-        // io.to(`user:${video.user_id}`).emit("video_process_start");
-    } catch (err) {
-        console.error("[PROCESS SERVICE] Video processing request failed");
-        console.error(err);
-
-        await prisma.video.update({
-            where: { id: video.id },
-            data: { status: "FAILED" },
-        });
-
-        io.to(`user:${video.user_id}`).emit("video_process_failed");
-    }
-
-    // // Remplacez ces valeurs par les données appropriées
-    // const apiUrl = process.env.PROCESS_URL!; // L'URL de l'API de téléchargement
-    // const filePath = `${__dirname}/../../uploads/originals/${video.id}.mp4`; // Le chemin vers votre fichier
-    // const videoId = video.id; // L'identifiant de la vidéo
-
-    // // Créez un objet FormData
-    // const formData = new FormData();
-
-    // // Ajoutez le fichier et le video_id au formulaire
-    // formData.set("video_id", videoId);
-    // formData.set("video", new Blob([await readFile(filePath)]));
-
-    // console.log("[PROCESS SERVICE] Requesting video processing...");
-
-    // /**
-    //  * Request video process
-    //  */
-
-    // try {
-    //   const response = await retry(
-    //     async (context) => {
-    //       // console.log(context.attemptNum, context.attemptsRemaining);
-
-    //       return fetch(apiUrl, {
-    //         method: "POST",
-    //         body: formData,
-    //       });
-    //     },
-    //     {
-    //       delay: 2000,
-    //       maxAttempts: 10,
-    //       factor: 2,
-    //       maxDelay: 10000,
-    //     }
-    //   );
-
-    //   console.log("[PROCESS SERVICE] Video processing request success");
-
-    //   await prisma.video.update({
+    // await prisma.video.update({
     //     where: { id: video.id },
     //     data: { status: "PROCESSING" },
-    //   });
+    // });
 
-    //   io.to(`user:${video.user_id}`).emit("video_process_start");
-    // } catch (err) {
-    //   console.error("[PROCESS SERVICE] Video processing request failed");
-    //   console.error(err);
+    // io.to(`user:${video.user_id}`).emit("video_process_start");
+  } catch (err) {
+    console.error("[PROCESS SERVICE] Video processing request failed");
+    console.error(err);
 
-    //   await prisma.video.update({
-    //     where: { id: video.id },
-    //     data: { status: "FAILED" },
-    //   });
+    await prisma.video.update({
+      where: { id: video.id },
+      data: { status: "FAILED" },
+    });
 
-    //   io.to(`user:${video.user_id}`).emit("video_process_failed");
-    // }
+    io.to(`user:${video.user_id}`).emit("video_process_failed");
+  }
+
+  // // Remplacez ces valeurs par les données appropriées
+  // const apiUrl = process.env.PROCESS_URL!; // L'URL de l'API de téléchargement
+  // const filePath = `${__dirname}/../../uploads/originals/${video.id}.mp4`; // Le chemin vers votre fichier
+  // const videoId = video.id; // L'identifiant de la vidéo
+
+  // // Créez un objet FormData
+  // const formData = new FormData();
+
+  // // Ajoutez le fichier et le video_id au formulaire
+  // formData.set("video_id", videoId);
+  // formData.set("video", new Blob([await readFile(filePath)]));
+
+  // console.log("[PROCESS SERVICE] Requesting video processing...");
+
+  // /**
+  //  * Request video process
+  //  */
+
+  // try {
+  //   const response = await retry(
+  //     async (context) => {
+  //       // console.log(context.attemptNum, context.attemptsRemaining);
+
+  //       return fetch(apiUrl, {
+  //         method: "POST",
+  //         body: formData,
+  //       });
+  //     },
+  //     {
+  //       delay: 2000,
+  //       maxAttempts: 10,
+  //       factor: 2,
+  //       maxDelay: 10000,
+  //     }
+  //   );
+
+  //   console.log("[PROCESS SERVICE] Video processing request success");
+
+  //   await prisma.video.update({
+  //     where: { id: video.id },
+  //     data: { status: "PROCESSING" },
+  //   });
+
+  //   io.to(`user:${video.user_id}`).emit("video_process_start");
+  // } catch (err) {
+  //   console.error("[PROCESS SERVICE] Video processing request failed");
+  //   console.error(err);
+
+  //   await prisma.video.update({
+  //     where: { id: video.id },
+  //     data: { status: "FAILED" },
+  //   });
+
+  //   io.to(`user:${video.user_id}`).emit("video_process_failed");
+  // }
 };
